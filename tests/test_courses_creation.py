64255c062a446e9da86d9fc69754a786d60f6b7b
Coeus <vladimir.shishkin@kirarock.team>
2025-05-13 13:41:41 +0300

    feat: parametrize course test

--- tests/test_courses_creation.py
+++ tests/test_courses_creation.py
@@ -8,25 +8,31 @@ from tools.routes import AppRoute
 @pytest.mark.regression
 @pytest.mark.registration
 class TestCoursesCreation:
+    data = [
+        (
+            "First course test",
+            "10",
+            "Test description",
+            "100",
+            "90"
+        )
+    ]
+
+    @pytest.mark.parametrize("data", data)
     @allure.title("Successful course creation")
     def test_successful_course_creation(
             self,
             courses_page: CoursesPage,
+            data: tuple
     ):
         courses_page.visit(AppRoute.COURSES)
         courses_page.courses_toolbar_view.check_visible()
         courses_page.click_create_button()
 
         courses_page.create_course_form.check_visible(title="", estimated_time="", description="", max_score="0", min_score="0")
-        courses_page.create_course_form.fill(
-            title="First course test",
-            estimated_time="10",
-            description="Test description",
-            max_score="100",
-            min_score="90"
-        )
+        courses_page.create_course_form.fill(*data)
         courses_page.create_course_form.click_save_button()
 
-        courses_page.check_visible_created_course(title="First course test")
+        courses_page.check_visible_created_course(title=data[0])
 
         # TODO a lot of courses creation and other tests
